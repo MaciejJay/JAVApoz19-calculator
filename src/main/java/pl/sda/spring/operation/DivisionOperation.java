d77572911adedb806829439321c5b08e56b96076
Maciej Jasiński <maciejj95@op.pl>
2020-01-26 11:33:09 +0100

    changed exception handler

--- src/main/java/pl/sda/spring/operation/DivisionOperation.java
+++ src/main/java/pl/sda/spring/operation/DivisionOperation.java
@@ -5,12 +5,14 @@ import pl.sda.spring.exception.DivideByZeroException;
 
 @Component
 public class DivisionOperation implements Operation {
+
     @Override
     public double calculate(double arg1, double arg2) throws DivideByZeroException {
         if (arg2 != 0) {
             return arg1 / arg2;
         } else {
-            throw new DivideByZeroException("You cannot divide by zero.");
+            String errMessage = "You cannot divide by zero.";
+            throw new DivideByZeroException(errMessage);
         }
     }
 
